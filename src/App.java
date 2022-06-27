01d9f245d573ab9dab6e28ea68183446c7cca730
zvb <zvbmegasoft@gmail.com>
2022-06-27 14:44:38 +0300

    заработало

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Привет, Маша!");
+        System.out.println("Ну и как тебе java?");
+        System.out.println("Нравиться?");
+    }
+}
