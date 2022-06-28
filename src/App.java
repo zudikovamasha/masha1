136dfd6da0e63c924c9808233759ae76dd1bdc71
zvb <zvbmegasoft@gmail.com>
2022-06-28 17:59:32 +0300

    Задание 3 (готово)

--- src/App.java
+++ src/App.java
@@ -5,14 +5,13 @@ public class App {
         System.out.printf("%n Введите точность (0;1): ");
         Scanner in = new Scanner(System.in);
         double accuracy = in.nextDouble();
+        in.close ();
         if ((accuracy <= 0.0) | (accuracy >= 1.0)) {
             System.out.printf("%n Ошибка: неверно задана точность%n");
             return;
         }
         long k, k1, k2 = 0, kmax = 2147483647;
         double s2 = 0.0, s1, a = 0.0, a1, a2;
-        System.out.printf(" Введите значение x: ");
-        int x = in.nextInt();
         for (k = 1; k <= kmax; k++) {
             k2 = k * 2;
             k1 = k2 - 1;
@@ -30,6 +29,5 @@ public class App {
             System.out.printf("%n Предупреждение: заданная точность не достигнута%n");
         System.out.printf("%n Сумма ряда %f Кол-во слагаемых %d Точность %f%n", s2, k2, (a < 0.0) ? -a : a);
         System.out.printf(" Сумма ряда %f Кол-во слагаемых %d (ряд не сходится)%n%n", s1, k1);
-        in.close();
     }
 }
