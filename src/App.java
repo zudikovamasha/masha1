881dc891c47751c5fe5ae4ffe7572e62f29aac2f
zvb <zvbmegasoft@gmail.com>
2022-08-22 12:22:09 +0300

    копия перед сносом

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -10,24 +11,26 @@ public class App {
             System.out.printf("%n Ошибка: неверно задана точность%n");
             return;
         }
-        long k, k1, k2 = 0, kmax = 2147483647;
-        double s2 = 0.0, s1, a = 0.0, a1, a2;
-        for (k = 1; k <= kmax; k++) {
-            k2 = k * 2;
-            k1 = k2 - 1;
-            a2 = (k2 * 2.0 + 100.0) / (k2 * 3.0 + 1.0);
-            a1 = (k1 * 2.0 + 100.0) / (k1 * 3.0 + 1.0);
+        double i=1, imax = 2147483647;
+        double s2 = 0.0, s1, a = 0.0, a1, a2, h;
+        double is=Math.sqrt(i);
+        double i1s=Math.sqrt(i-1);
+        double c = -1;
+        double ps=Math.pow(c, i);
+        for (i = 1; i <= imax; i++) {
+            h = -1*((is*(i-1))/(i*i1s));
+            a1 = ps * (is/i);
+            a2 = a1 * h;
             a = a2 - a1;
             s2 += a;
             if ((-accuracy <= a) & (a <= accuracy))
                 break;
         }
-        k1 = k2 + 1;
-        a1 = (k1 * 2.0 + 100.0) / (k1 * 3.0 + 1.0);
+        a1 = ps * (is/i);
         s1 = s2 - a1;
-        if (k > kmax)
+        if (i > imax)
             System.out.printf("%n Предупреждение: заданная точность не достигнута%n");
-        System.out.printf("%n Сумма ряда %f Кол-во слагаемых %d Точность %f%n", s2, k2, (a < 0.0) ? -a : a);
-        System.out.printf(" Сумма ряда %f Кол-во слагаемых %d (ряд не сходится)%n%n", s1, k1);
+        System.out.printf("%n Сумма ряда %f Кол-во слагаемых %d Точность %f%n", s2, i, (a < 0.0) ? -a : a);
+        System.out.printf(" Сумма ряда %f Кол-во слагаемых %d (ряд не сходится)%n%n", s1, i);
     }
 }
